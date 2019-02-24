4f88a29152d73cbe94c59b35f9c5cb1d158e6919
Falco Winkler <falco.winkler@esailors.de>
2019-02-24 11:12:14 +0000

    no i can not

--- src/main/scala/de/haw/tweetspace/Recommender.scala
+++ src/main/scala/de/haw/tweetspace/Recommender.scala
@@ -1,21 +1,23 @@
 package de.haw.tweetspace
 
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.spark.sql.SparkSession
 
 object Recommender extends LazyLogging {
   def main(args: Array[String]): Unit = {
-    val spark = HDFSHelper.spark
+
+    val spark: SparkSession = SparkSession.builder
+      .appName(AppConfig.value("app.name").get)
+      .master(AppConfig.value("spark.master").get)
+      .getOrCreate()
+
     val basePath = AppConfig.value("hdfs.uri").get + "/"
     val tweets = UserDataSet.load(spark, basePath + "gobblin-kafka-avro/job-output/user_tweets")
       .select("twitter_user_id", "in_reply_to_twitter_user_id")
     val registrations = UserDataSet.load(spark, basePath + "gobblin-kafka-avro/job-output/user_registrations")
       .select("twitter_user_id", "name")
     val joined = UserDataFunctions.join(tweets, registrations)
-
     val kafkaProducerReady = UserDataFunctions.mapToKafkaProducerRecord(joined)
-    UserDataFunctions.publishToKafa(kafkaProducerReady)
-
-    joined.show(10)
-    logger.info("Successfully did stuff: " + joined.count)
+    UserDataFunctions.publishToKafka(kafkaProducerReady)
   }
 }
--- src/main/scala/de/haw/tweetspace/UserDataFunctions.scala
+++ src/main/scala/de/haw/tweetspace/UserDataFunctions.scala
@@ -6,6 +6,8 @@ import io.confluent.kafka.serializers.KafkaAvroSerializer
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, Row}
 import org.joda.time.DateTime
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.types.{DataTypes, StructType}
 
 object UserDataFunctions {
   def join(tweetData: DataFrame, registrationData: DataFrame): DataFrame = {
@@ -13,15 +15,11 @@ object UserDataFunctions {
   }
 
   def mapToKafkaProducerRecord(joinedDataFrame: DataFrame): DataFrame = {
-    import org.apache.spark.sql.catalyst.encoders.RowEncoder
-    import org.apache.spark.sql.types.{DataTypes, StructType}
+    // Spark needs a row encoder for data serialization.
+    // Here we explicitly state that we want the map function to produce a BinaryType
     var structType = new StructType
-    structType = structType.add("value", DataTypes.BinaryType, false)
+    structType = structType.add("value", DataTypes.BinaryType, nullable = false)
     val encoder = RowEncoder(structType)
-
-    val registryClient = new CachedSchemaRegistryClient(
-      AppConfig.value("schema_registry.url").get,
-      256)
 
     def getValues(row: Row): Row = {
       val specificRecord = FriendReccomendation.newBuilder
@@ -30,17 +28,24 @@ object UserDataFunctions {
         .setReccomendationReceiverName(row.getString(2))
         .setMatchPercentage(0.5F)
         .setTimestamp(DateTime.now()).build()
+      // TODO: caching?
+      val registryClient = new CachedSchemaRegistryClient(
+        AppConfig.value("schema_registry.url").get,
+        256)
       val serializer = new KafkaAvroSerializer(registryClient)
       registryClient.register("friend_recommendations-value", specificRecord.getSchema)
       Row(serializer.serialize("friend_recommendations", specificRecord))
     }
 
-    joinedDataFrame.filter(col("in_reply_to_twitter_user_id").isNotNull)
+    joinedDataFrame
+      .filter(col("in_reply_to_twitter_user_id").isNotNull)
       .map(getValues, encoder)
+      // Key column is the kafka key, which is used for partitioning,
+      // Not necessarily needed so we just specify a constant here
       .withColumn("key", lit(""))
   }
 
-  def publishToKafa(dataFrame: DataFrame): Unit = {
+  def publishToKafka(dataFrame: DataFrame): Unit = {
     dataFrame.write.
       format("kafka")
       .option("kafka.bootstrap.servers", AppConfig.value("kafka.bootstrap_servers").get)
